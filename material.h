7586e6155e670a0bfec215c2e422bce02ba594fd
Baptiste NICOLET <baptiste.nicolet@polytechnique.edu>
2018-10-08 18:37:25 +0200

    Added Glass material

--- material.h
+++ material.h
@@ -17,6 +17,26 @@ vec3 reflect(const vec3& v, const vec3& n){
     return v - 2*dot(v,n)*n;
 }
 
+bool refract(const vec3& v, const vec3& n, float n1_over_n2, vec3& refracted_ray){
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    float vertical_refraction = 1.0f - (n1_over_n2*n1_over_n2)*(1.0f - dt*dt); // coordinate of the refracted ray along the normal
+
+    if(vertical_refraction > 0){
+        //if the ray is indeed refracted (according to Snell-Descartes' law)
+        refracted_ray = -sqrt(vertical_refraction)*n + n1_over_n2*(uv - dt*n);
+        return true;
+    }
+    //else
+    return false;
+}
+
+float schlick(float cos, float ref_index){
+    //compute the Schlick approximation of the reflection factor
+    float r0 = (1.0f - ref_index) / (1.0f + ref_index);
+    r0 = r0*r0;
+    return r0 + (1-r0)*pow((1-cos),5);
+}
 
 class material{
     public:
@@ -52,6 +72,50 @@ class metal : public material{
             return dot(scattered.direction(), rec.normal) >0;
         }
 
+};
+
+class dielectric : public material{
+    public:
+        float ref_index;
+
+        dielectric(float r) : ref_index(r) {}
+
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const{
+            float n1_over_n2;
+            vec3 refracted;
+            vec3 outward_normal;
+            float ref_prob, cos;
+
+            attenuation = vec3(1.0f, 1.0f, 1.0f);
+
+            if(dot(r_in.direction(), rec.normal) > 0){//if it is an internal ray
+                outward_normal = -rec.normal;
+                n1_over_n2 = ref_index;
+                cos = dot(r_in.direction(), rec.normal) / r_in.direction().length();
+                cos = sqrt(1 - ref_index*ref_index*(1-cos*cos));
+            }
+            else{
+                outward_normal = rec.normal;
+                n1_over_n2 = 1.0f / ref_index;
+                cos = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+            }
+
+            if (refract(r_in.direction(), outward_normal, n1_over_n2, refracted)){
+                ref_prob = schlick(cos, ref_index);
+            }
+            else{
+                ref_prob =1.0f;
+            }
+            if(drand48() < ref_prob){
+                vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);//reflexion
+                //reflected+=fuzz*random_unit_vector();//add a little randomness to the reflection, will create some blur
+                scattered = ray(rec.p, reflected);
+            }
+            else{
+                scattered = ray(rec.p, refracted);
+            }
+            return true;
+        }
 };
 
 #endif // MATERIAL_H
